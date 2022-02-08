bb9ec178d7671cee17f9686a8e151dd30d262505
Naqash <naqashahmed471@gmail.com>
2022-02-08 23:46:43 +0500

    conditional rendering in navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ import logo from "../public/rupee.png";
 import banner from "../public/banner.png";
 
 const Navbar = () => {
-  const { user, login, logout } = useContext(AuthContext);
+  const { user, login, logout, authReady } = useContext(AuthContext);
   console.log(user);
 
   return (
@@ -14,24 +14,31 @@ const Navbar = () => {
       <nav>
         <Image src={logo} width={50} height={48} alt="logo" />
         <h1>Gaming Vibes</h1>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/guides">
-              <a>Guides</a>
-            </Link>
-          </li>
-          <li onClick={login} className="btn">
-            Login/Signup
-          </li>
-          <li onClick={logout} className="btn">
-            Log out
-          </li>
-        </ul>
+        {authReady && (
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/guides">
+                <a>Guides</a>
+              </Link>
+            </li>
+            {!user && (
+              <li onClick={login} className="btn">
+                Login/Signup
+              </li>
+            )}
+            {user && <li>{user.email}</li>}
+            {user && (
+              <li onClick={logout} className="btn">
+                Log out
+              </li>
+            )}
+          </ul>
+        )}
       </nav>
       <div className="banner">
         <Image src={banner} width={966} height={276} alt="banner" />
