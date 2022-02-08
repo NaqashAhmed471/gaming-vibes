02813b05350cb12ad65d479ac304b67978166a19
Naqash <naqashahmed471@gmail.com>
2022-02-09 01:06:26 +0500

    add netlify functions

--- pages/guides.js
+++ pages/guides.js
@@ -1,6 +1,14 @@
-import styles from "../styles/Guides.module.css"
+import styles from "../styles/Guides.module.css";
+import { useEffect } from "react";
 
 const Guides = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("");
+      const data = await res.json();
+    };
+    fetchData();
+  }, []);
   return (
     <div className={styles.guides}>
       <h2>All Guides</h2>
